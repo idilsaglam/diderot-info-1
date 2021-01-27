56954bc8e397cc6e0da0a61bf64b8c07e091fa56
idilsaglam <idilsaglam1@hotmail.com>
2021-01-28 00:06:09 +0100

    Test updated

--- Cours/2020/IP2/TP/TP1/src/Test.java
+++ Cours/2020/IP2/TP/TP1/src/Test.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Test {
 
     public static void main(String[] args){
@@ -5,8 +7,24 @@ public class Test {
         Fruit g = new Fruit("pamplemousse",330);
         Fruit h=f;
         System.out.println("Test termine");
-        f.afficher();
-        Fruit.hybridation(new Fruit("Pomme",10),new Fruit("Banane",2)).afficher();
+        Fruit.afficher(f);
+        Fruit.afficher(Fruit.hybridation(new Fruit("Pomme",10),new Fruit("Banane",2)));
+
+        Panier.afficher(
+
+                new Panier(new Fruit[]{
+                        new Fruit("Pomme",20),
+                        new Fruit("Poire",30)
+                }
+                )
+         );
+        System.out.println(Panier.hybridePanier(
+                new Fruit("Banane",10),
+                new Panier(new Fruit[]{
+                        new Fruit("Ananas",40),
+                        new Fruit("Poire",20),
+                        new Fruit("Pomme",10)}
+                )));
     }
 }
 
