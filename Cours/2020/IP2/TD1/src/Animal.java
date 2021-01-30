030ece138782d4ca9f85dfaa45eb2892963cb92a
idilsaglam <idilsaglam1@hotmail.com>
2021-01-30 01:01:58 +0100

    Animal updated

--- Cours/2020/IP2/TD1/src/Animal.java
+++ Cours/2020/IP2/TD1/src/Animal.java
@@ -6,14 +6,25 @@ public int poids;
 public String espece;
 public int faim;
 
-Animal(String nom, char sexe, int jours,int poids, String espece,int faim){
+Animal(String nom, char sexe, int jours,int poids, String espece) {
     this.nom = nom;
     this.sexe = sexe;
     this.jours = jours;
     this.poids = poids;
     this.espece = espece;
-    this.faim = (faim % 11);
 }
+/* Dans l'exercices précedents (l'exercice 3) il me demande de créer des animaux sans préciser le niveau de faim. Pour pouvoir ajouter un attribut 'faim'
+   et pour ne pas impecter les exercices précedents au lieu de ajouter un attribut 'faim' dans le constructeur j'ai crée deuxiéme constructeur.
+ */
+    Animal(String nom, char sexe, int jours,int poids, String espece,int faim) {
+        this.nom = nom;
+        this.sexe = sexe;
+        this.jours = jours;
+        this.poids = poids;
+        this.espece = espece;
+        this.faim = faim;
+    }
+
     public static void description(Animal animal){
     String article,sex;
         if(animal.sexe == 'm'){
@@ -23,7 +34,7 @@ Animal(String nom, char sexe, int jours,int poids, String espece,int faim){
             sex = "femelle";
             article = "e";
         }
-        System.out.println("Je m'appelle " + animal.nom + ", Je suis un" +article+ " "+ animal.espece + " " + sex + ", j'ai " + ""+animal.jours + " jours et je pese " + animal.poids + " kg.");
+        System.out.printf("Je m'appelle %s Je suis un %s %s %s ,j'ai %d jours et je pese %d kg.\n",animal.nom,article,animal.espece,sex,animal.jours,animal.poids);
     }
 
     public static void descriptionBis(Animal animal) {
@@ -31,7 +42,7 @@ Animal(String nom, char sexe, int jours,int poids, String espece,int faim){
         if (animal.sexe == 'm') {
             sex = "male";
             article = "";
-            System.out.println("Je m'appelle " + animal.nom + ", Je suis un" + article + " " + animal.espece + " " + sex + ", j'ai " + "" + animal.jours / 365 + " ans " + animal.jours % 365 + " jours " + " et je pese" + animal.poids + " kg.");
+            System.out.printf("Je m'appelle %s Je suis un %s %s %s ,j'ai %d ans %d jours et je pese %d kg.\n",animal.nom,article,animal.espece,sex,animal.jours/365,animal.jours%365,animal.poids);
         }
     }
 
