1a4d6d2a0172ad142081db1cc431a6bebf25ba05
idilsaglam <idilsaglam1@hotmail.com>
2021-01-30 01:02:30 +0100

    Main updated

--- Cours/2020/IP2/TD1/src/Main.java
+++ Cours/2020/IP2/TD1/src/Main.java
@@ -14,21 +14,22 @@ public class Main {
         return max;
     }
 
-    public static Animal reproduction(Animal a, Animal b){
+    /*public static Animal reproduction(Animal a, Animal b){
         if(a.sexe != b.sexe && a.faim<5 && b.faim<5){
-            return new Animal(a.nom+b.nom,'',)
+            return new Animal(a.nom+b.nom,'m')
         }
     }
+     */
 
     public static void main(String[] args){
         //créer un zèbre mâle de 5 ans, pesant 300 kg s’appelant Marti
-        Animal d = new Animal("Marti",'m',5*365,300,"zebre",0);
+        Animal Marti = new Animal("Marti",'m',5*365,300,"zebre");
 
         //hippopotame femelle de 7 ans, d’une tonne et demie s’appelant Gloria
-        Animal e = new Animal("Gloria",'f',7*365,1500,"hippopotame",0);
+        Animal Gloria = new Animal("Gloria",'f',7*365,1500,"hippopotame");
 
         //Pour diminiuer le poids de Gloria de 50 kg (Question 4)
-        e.poids -= 50;
+        Gloria.poids -= 50;
 
         //Question 5
         Animal a = new Animal("Melman",'m',1230,998,"girafe",0);
@@ -41,10 +42,12 @@ public class Main {
         Animal.descriptionBis(new Animal("Rico",'m',1129,30,"manchot",0));
         System.out.println(lePlusGros(
                 new Animal[]{
-                        new Animal("lolo",'f',300,20,"girafe",3),
-                        new Animal("tutu",'m',2000,6500,"manchot",2),
-                        new Animal("lala",'f',900,204,"zebre",5),
-                }));
+                        new Animal("lolo", 'f', 300, 20, "girafe", 3),
+                        new Animal("tutu", 'm', 2000, 6500, "manchot", 2),
+                        new Animal("lala", 'f', 900, 204, "zebre", 5),
+                           })
+        );
+
     }
 
 
